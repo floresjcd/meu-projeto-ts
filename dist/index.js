91b1660eea4a3404a3f0d1ac01903eac48d752c0
José Carlos Flores <30850074+floresjcd@users.noreply.github.com>
2026-04-24 16:12:24 -0300

    Add files via upload

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,5 @@
 "use strict";
-function saudar(nome) {
-    return `Olá, ${nome}! Bem-vindo(a) ao TypeScript.`;
-}
-let usuario = "Estudante";
-console.log(saudar(usuario));
-// Tente introduzir um erro de tipo para ver o VS Code e o compilador agindo:
-// usuario = 123; // Isso mostrará um erro no VS Code
+Object.defineProperty(exports, "__esModule", { value: true });
+let saudacao = "Olá, acadêmicos de Engenharia de Software e ADS!";
+console.log(saudacao);
+//# sourceMappingURL=index.js.map
